357cfb1d01db7fb16128bde1b9537dc57ee541f2
NguyenKhoi04 <ngkhoi04@gmail.com>
2026-03-15 09:43:17 +0700

    59

--- app/api/projects/[projectId]/members/list/route.ts
+++ app/api/projects/[projectId]/members/list/route.ts
@@ -20,7 +20,7 @@ export async function GET(
     });
 
     const items = rows
-      .map((r) => ({
+      .map((r: (typeof rows)[number]) => ({
         id: r.user.id,
         name: r.user.name,
         email: r.user.email!,
