8a0134cf431a2f784c411b722761c631b9d7737c
NguyenKhoi04 <ngkhoi04@gmail.com>
2026-03-15 10:56:48 +0700

    74

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,30 +1,17 @@
-// // lib/prisma.ts
-
-import { PrismaClient, Prisma, $Enums } from "@prisma/client"
+// lib/prisma.ts
+import { PrismaClient, Prisma, $Enums } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient()
-
-if (process.env.NODE_ENV !== "production")
-  globalForPrisma.prisma = prisma
-
 // Re-export useful Prisma types and enums for the rest of the app
-export { Prisma, $Enums }
-export type ActivityType = $Enums.ActivityType
-export const SprintStatus = $Enums.SprintStatus
-
-
-// import { PrismaClient } from '../app/generated/prisma';
-
-// const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-// export const prisma =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({ log: ['error', 'warn'] });
-
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export { Prisma, $Enums };
+export type ActivityType = $Enums.ActivityType;
+export const SprintStatus = $Enums.SprintStatus;
